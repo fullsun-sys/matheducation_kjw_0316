be27d201c13c5bd5f659b86c1b4c23a92ed5f79a
fullsun-sys <fullsun@inu.ac.kr>
2026-03-16 13:58:47 +0000

    자기소개서

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,13 @@
 import streamlit as st
-import os
+
+st.header("안녕하세요! 👋")
 
 st.title("자기소개서")
+
 st.write("성명: 김지원")
 st.write("생년월일: 2003년 4월 9일")
 st.write("학교: 인천대학교")
 st.write("학과: 수학교육과")
-st.markdown("---")
-st.header("안녕하세요! 👋")
 
-# 페이지 선택
-page = st.sidebar.selectbox("페이지 선택", ["메인", "자기소개서"])
-
-if page == "자기소개서":
-	st.subheader("간단 소개")
-	st.write("저는 인천대학교 수학교육과 재학 중인 김지원입니다. 수학, 논리적 사고에 관심이 많습니다.")
+st.subheader("간단 소개")
+st.write("저는 인천대학교 수학교육과 재학 중인 김지원입니다. 수학, 논리적 사고에 관심이 많습니다.")
